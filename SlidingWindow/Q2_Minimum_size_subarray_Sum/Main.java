8ccf51ab414faf2ad8d19c4fc84fa3927c22e8d4
Chen Zhu DeskTop_US <zhuchen.2001@icloud.com>
2024-11-06 23:27:54 -0500

    minmum size subarray sum finished

--- SlidingWindow/Q2_Minimum_size_subarray_Sum/Main.java
+++ SlidingWindow/Q2_Minimum_size_subarray_Sum/Main.java
@@ -2,31 +2,44 @@ package SlidingWindow.Q2_Minimum_size_subarray_Sum;
 
 import java.util.Arrays;
 
-class MinimumSubArraySum{
+class MinimumSubArraySum {
     public static int minSubArrayLen(int target, int[] nums) {
-       int min_Window_size = Integer.MAX_VALUE; 
-       for(int i =0; i<nums.length; i++){
-            
+        int windowSize = Integer.MAX_VALUE;
+        int currSubArrSize = 0;
+        int start = 0;
+        int sum = 0;
 
+        for (int end = 0; end < nums.length; end++) {
+            sum += nums[end];
+            while (sum >= target) {
+                currSubArrSize = end - start + 1;
+                windowSize = Math.min(windowSize, currSubArrSize);
+                sum -= nums[start];
+                start++;
             }
-          
-       return -1;
+        }
+        if (windowSize != Integer.MAX_VALUE) {
+            return windowSize;
+        } else {
+            return 0;
+
+        }
     }
- }
+}
 
 public class Main {
-     public static void main(String[] args) {
-        int[] target = {7, 4, 11, 10, 5, 15};
+    public static void main(String[] args) {
+        int[] target = { 7, 4, 11, 10, 5, 15 };
         int[][] inputArr = {
-            {2, 3, 1, 2, 4, 3},
-            {1, 4, 4},
-            {1, 1, 1, 1, 1, 1, 1, 1},
-            {1, 2, 3, 4},
-            {1, 2, 1, 3},
-            {5, 4, 9, 8, 11, 3, 7, 12, 15, 44}
+                { 2, 3, 1, 2, 4, 3 },
+                { 1, 4, 4 },
+                { 1, 1, 1, 1, 1, 1, 1, 1 },
+                { 1, 2, 3, 4 },
+                { 1, 2, 1, 3 },
+                { 5, 4, 9, 8, 11, 3, 7, 12, 15, 44 }
         };
         for (int i = 0; i < target.length; i++) {
-            int windowSize = minSubArrayLen(target[i], inputArr[i]);
+            int windowSize = MinimumSubArraySum.minSubArrayLen(target[i], inputArr[i]);
             System.out.print((i + 1) + ".\tInput array: " + Arrays.toString(inputArr[i]));
             System.out.print("\n\tTarget: " + target[i]);
             System.out.println("\n\tMinimum Length of Subarray: " + windowSize);
