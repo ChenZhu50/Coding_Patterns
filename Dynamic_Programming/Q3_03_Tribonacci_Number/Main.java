3f8bf94ab6f69667fc5163ea1e28ffa235eac50e
Chen Zhu DeskTop_US <zhuchen.2001@icloud.com>
2024-12-01 13:45:12 -0500

    DP Q3 tribonacci question complete, solution two done

--- Dynamic_Programming/Q3_03_Tribonacci_Number/Main.java
+++ Dynamic_Programming/Q3_03_Tribonacci_Number/Main.java
@@ -12,28 +12,52 @@ public class Main {
 			System.out.println(new String(new char[100]).replace('\0', '-'));
 		}
 	}
+	/*
+	 * Solution one:
+	 * public static int findTribonacci(int n) {
+	 * int[] dp = new int[n + 1];
+	 * Arrays.fill(dp, -1);
+	 * return findTribonacciHelper(n, dp);
+	 * 
+	 * }
+	 * 
+	 * public static int findTribonacciHelper(int n, int dp[]) {
+	 * // base case:
+	 * if (n == 0)
+	 * return 0;
+	 * if (n == 1)
+	 * return 1;
+	 * if (n == 2)
+	 * return 1;
+	 * if (dp[n - 1] != -1) {
+	 * return dp[n];
+	 * }
+	 * 
+	 * dp[n] = findTribonacciHelper(n - 1, dp) + findTribonacciHelper(n - 2, dp) +
+	 * findTribonacciHelper(n - 3, dp);
+	 * 
+	 * return dp[n];
+	 * }
+	 */
+
+	// solution 2: replace:
 
 	public static int findTribonacci(int n) {
-		int[] dp = new int[n + 1];
-		Arrays.fill(dp, -1);
-		return findTribonacciHelper(n, dp);
-
-	}
-
-	public static int findTribonacciHelper(int n, int dp[]) {
 		// base case:
-		if (n == 0)
-			return 0;
-		if (n == 1)
-			return 1;
-		if (n == 2)
-			return 1;
-		if (dp[n - 1] != -1) {
-			return dp[n];
+		if (n < 3)
+			// 只有当 n = 0 时，我们才会返回0，如果是1或者2都是返回1的
+			return n == 0 ? 0 : 1;
+
+		int first = 0, second = 1, third = 1;
+		int temp = 0;
+
+		//之后，我们只需要替换就好了，因为我们也并没有需要前面的数字，只是简单的当前三个数字相加而已。
+		for (int i = 3; i < n; i++) {
+			temp = first + second + third;
+			first = second;
+			second = third;
+			third = temp;
 		}
-
-		dp[n] = findTribonacciHelper(n - 1, dp) + findTribonacciHelper(n - 2, dp) + findTribonacciHelper(n - 3, dp);
-
-		return dp[n];
+		return temp;
 	}
 }
