b24cb3b1f68c95d65272244bab482aade861b21e
Chen Zhu <zhucoco414@gmail.com>
2024-12-04 23:40:21 -0500

    Strobogrammatic done, my own!

---
+++ Two_pointers/Q2_Strobogrammatic Number/Main.java
@@ -0,0 +1,42 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] nums = {
+            "609",
+            "88",
+            "962",
+            "101",
+            "123"
+        };
+
+        int i = 0;
+        for (String num : nums) {
+            System.out.println((i + 1) + ".\tnum: " + num);
+            System.out.println("\n\tIs strobogrammatic: " + (isStrobogrammatic(num) ? "true" : "false"));
+            System.out.println(new String(new char[100]).replace("\0", "-"));
+            i++;
+        }
+    }
+    public static boolean isStrobogrammatic(String num) {
+        int front = 0, end = num.length() - 1;
+        char left, right;
+
+        while(end > front){
+            left = num.charAt(front);
+            right = num.charAt(end);
+            front++;
+            end--;
+
+
+            if((left == 6 && right == 9)||(left == 9 && right == 6)){
+                continue;
+            }
+            else if ((left == '0' && right == '0') || (left == '1' && right == '1') || (left == '8' && right == '8')) {
+                continue;
+            } else {
+                return false;
+            }
+        }
+        
+        return true;    
+    }
+}
