74ddc9fae1e94ba01d9fd6d08256137b4a50ff10
Chen Zhu <zhucoco414@gmail.com>
2024-12-06 23:19:59 -0500

    Q3 done

--- Two_pointers/Q3_Minimum_Number_of_Moves_to_Make_Palindrome/Main.java
+++ Two_pointers/Q3_Minimum_Number_of_Moves_to_Make_Palindrome/Main.java
@@ -13,18 +13,40 @@ public class Main {
             System.out.println(new String(new char[100]).replace("\0", "-"));
         }
     }
-         /*
-         todo: 1. 初始化moves为0
-         todo: 2. 初始化两个指针，一个指向字符串的头部，一个指向字符串的尾部
-         todo：3. 使用while循环，只要i>j，就一直循环
-         todo：4. 在内部循环中，从j处从后往前查找，找到一个和i处相同的字符
-         todo：5. 如果找到了，交换相邻的字符到其右侧，直到达到j的位置，同时moves++
-         todo：6. 如果没有找到，增加需要的moves来讲i处的字符移到j处
-         todo：7. 当我们完成了所有对的修改，将i指针向前移动，j指针向后移动
-         todo：8. 返回moves
-         */
+    /*
+     * todo: 1. 初始化moves为0
+     * todo: 2. 初始化两个指针，一个指向字符串的头部，一个指向字符串的尾部
+     * todo：3. 使用while循环，只要i>j，就一直循环
+     * todo：4. 在内部循环中，从j处从后往前查找，找到一个和i处相同的字符
+     * todo：5. 如果找到了，交换相邻的字符到其右侧，直到达到j的位置，同时moves++
+     * todo：6. 如果没有找到，增加需要的moves来讲i处的字符移到j处
+     * todo：7. 当我们完成了所有对的修改，将i指针向前移动，j指针向后移动
+     * todo：8. 返回moves
+     */
 
     public static int minMovesToMakePalindrome(String s) {
-        return 0;
+        // 因为有移动操作，所以需要String变回char array
+        char[] chars = s.toCharArray();
+
+        int moves = 0;
+        int i = 0, j = s.length() - 1;
+        int k = j;
+        for (; k > i; --k) {
+            if (chars[i] == chars[k]) {
+                for (; k < j; --k) {
+                    if (s.charAt(i) == s.charAt(k)) {
+                        char temp = chars[k];
+                        chars[k] = chars[k + 1];
+                        ++moves;
+                    }
+                }
+                --j;
+                break;
+            }
+            if (k == i) {
+                moves += chars.length / 2 - i;
+            }
+        }
+        return moves;
     }
 }
