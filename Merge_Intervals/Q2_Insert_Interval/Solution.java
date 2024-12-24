a0fa3814b3692cada38e35a50c95ea168fdeca15
Chen Zhu DeskTop_US <zhuchen.2001@icloud.com>
2024-12-23 23:29:27 -0500

    Q2 done with O(n)

--- Merge_Intervals/Q2_Insert_Interval/Solution.java
+++ Merge_Intervals/Q2_Insert_Interval/Solution.java
@@ -32,46 +32,109 @@ public class Solution {
         }
     }
 
+    /*
+     * current solution is okay, but O(N log N) is not the best solution
+     */
+    // public static int[][] insertInterval(int[][] existingIntervals, int[]
+    // newInterval) {
+    // // creat a linked list to store the merged intervals
+    // LinkedList<int[]> merged = new LinkedList<>();
+
+    // // still the base case, if null or empty, return empty array
+    // if (existingIntervals == null || existingIntervals.length == 0) {
+    // return new int[0][0];
+    // }
+
+    // // add the new interval to the existing intervals, and sort them by the first
+    // // element
+    // int[][] intervals = new int[existingIntervals.length + 1][2];
+    // for (int i = 0; i < existingIntervals.length; i++) {
+    // intervals[i] = existingIntervals[i];
+    // }
+    // intervals[existingIntervals.length] = newInterval;
+
+    // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    // // add the first interval to the merged list
+    // merged.add(intervals[0]);
+
+    // // loop through the intervals
+    // for (int i = 1; i < intervals.length; i++) {
+    // int[] interval = intervals[i];
+    // int[] lastAddedInterval = merged.getLast();
+
+    // int currStart = interval[0];
+    // int currEnd = interval[1];
+
+    // int lastEnd = lastAddedInterval[1];
+
+    // if (currStart <= lastEnd) {
+    // lastAddedInterval[1] = Math.max(lastEnd, currEnd);
+    // } else {
+    // merged.add(interval);
+    // }
+    // }
+
+    // return merged.toArray(new int[merged.size()][]);
+
+    // }
+
+    /*
+     * This solution is O(N) time complexity, which is better than the previous one
+     * 1. the intervals are sorted, we'll create an empty list to store the
+     * intervals before the new interval
+     * 2. add new interval to the list, and then merge the intervals if overlapping
+     * 3. add the merged intervals to the list
+     */
     public static int[][] insertInterval(int[][] existingIntervals, int[] newInterval) {
-        // creat a linked list to store the merged intervals
+        // todo: step 1: add existing intervals smaller then new interval
+        int newStart = newInterval[0];
+        int newEnd = newInterval[1];
+
+        // Initializing variable help iterating over the existing intervals
+        int i = 0;
+        int n = existingIntervals.length;
+
+        // Initializing empty list to store the output
         LinkedList<int[]> merged = new LinkedList<>();
 
-        // still the base case, if null or empty, return empty array
-        if (existingIntervals == null || existingIntervals.length == 0) {
-            return new int[0][0];
+        // as long as the current intervals that start before the new interval, append
+
+        while (i < n && existingIntervals[i][0] < newStart) {
+            merged.add(existingIntervals[i]);
+            i++;
         }
 
-        // add the new interval to the existing intervals, and sort them by the first
-        // element
-        int[][] intervals = new int[existingIntervals.length + 1][2];
-        for (int i = 0; i < existingIntervals.length; i++) {
-            intervals[i] = existingIntervals[i];
+        // todo: step 2: check if overlap for our new interval
+
+        // if the newStart < the end of last interval : append
+        // int currEnd = merged.get(merged.size()-1)[1]; //can't work, we need to make
+        // sure the merged size is > 0
+
+        if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < newStart) {
+            merged.add(newInterval);
+        } else {
+            // if we here, means there are overlap (currEnd > newStart)
+            // compare which end is bigger ? curr or the new interval's
+            merged.get(merged.size() - 1)[1] = Math.max(newEnd, merged.get(merged.size() - 1)[1]);
         }
-        intervals[existingIntervals.length] = newInterval;
 
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        // todo: step 3: handle the remaining intervals
+        while (i < n) {
+            int[] currVals = existingIntervals[i];
+            int start = currVals[0];
+            int end = currVals[1];
 
-        // add the first interval to the merged list
-        merged.add(intervals[0]);
+            // int endOfList = merged.get(merged.size()-1)[1];
 
-        // loop through the intervals
-        for (int i = 1; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            int[] lastAddedInterval = merged.getLast();
-
-            int currStart = interval[0];
-            int currEnd = interval[1];
-
-            int lastEnd = lastAddedInterval[1];
-
-            if (currStart <= lastEnd) {
-                lastAddedInterval[1] = Math.max(lastEnd, currEnd);
+            if (merged.get(merged.size() - 1)[1] < start) {
+                merged.add(currVals);
             } else {
-                merged.add(interval);
+                merged.get(merged.size() - 1)[1] = Math.max(end, merged.get(merged.size() - 1)[1]);
             }
+            i++;
         }
 
         return merged.toArray(new int[merged.size()][]);
-
     }
 }
