ea3a988b050c0d56e06d44b5cfab99f591e46211
Chen Zhu <zhucoco414@gmail.com>
2024-12-27 15:15:17 -0500

    finish analysis for Employee Free time, common added

--- Merge_Intervals/Q4_Employee_Free_Time/Main.java
+++ Merge_Intervals/Q4_Employee_Free_Time/Main.java
@@ -19,32 +19,69 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // 示例1
-        List<List<Interval>> example1 = Arrays.asList(
-                Arrays.asList(new Interval(1, 2), new Interval(5, 6)),
-                Arrays.asList(new Interval(1, 3)),
-                Arrays.asList(new Interval(4, 10)));
-        // 示例2
-        List<List<Interval>> example2 = Arrays.asList(
-                Arrays.asList(new Interval(1, 3), new Interval(6, 7)),
-                Arrays.asList(new Interval(2, 4)),
-                Arrays.asList(new Interval(2, 5), new Interval(9, 12)));
+        {
+            List<List<List<Interval>>> inputs1 = Arrays.asList(
+                    Arrays.asList(
+                            Arrays.asList(new Interval(1, 5)),
+                            Arrays.asList(new Interval(6, 10)),
+                            Arrays.asList(new Interval(4, 7))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(1, 3), new Interval(6, 7)),
+                            Arrays.asList(new Interval(2, 4)),
+                            Arrays.asList(new Interval(2, 5), new Interval(9, 12))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(2, 3), new Interval(7, 9)),
+                            Arrays.asList(new Interval(1, 4), new Interval(6, 7))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(3, 5), new Interval(8, 10)),
+                            Arrays.asList(new Interval(4, 6), new Interval(9, 12)),
+                            Arrays.asList(new Interval(5, 6), new Interval(8, 10))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(1, 3), new Interval(6, 9), new Interval(10, 11)),
+                            Arrays.asList(new Interval(3, 4), new Interval(7, 12)),
+                            Arrays.asList(new Interval(1, 3), new Interval(7, 10)),
+                            Arrays.asList(new Interval(1, 4)),
+                            Arrays.asList(new Interval(7, 10), new Interval(11, 12))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6),
+                                    new Interval(7, 8)),
+                            Arrays.asList(new Interval(2, 3), new Interval(4, 5), new Interval(6, 8))),
+                    Arrays.asList(
+                            Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6),
+                                    new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)),
+                            Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6),
+                                    new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)),
+                            Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6),
+                                    new Interval(7, 8), new Interval(9, 10), new Interval(11, 12)),
+                            Arrays.asList(new Interval(1, 2), new Interval(3, 4), new Interval(5, 6),
+                                    new Interval(7, 8), new Interval(9, 10), new Interval(11, 12))));
 
-        // 组合成一个列表，方便循环输出
-        List<List<List<Interval>>> inputs = Arrays.asList(example1, example2);
+            int i = 1;
+            List<List<List<Interval>>> inputs = new ArrayList<>();
 
-        int caseIndex = 1;
-        for (List<List<Interval>> schedule : inputs) {
-            System.out.println(caseIndex + ". Employee Schedules:");
-            for (int i = 0; i < schedule.size(); i++) {
-                System.out.println("\t" + display(schedule.get(i)));
+            for (int j = 0; j < inputs1.size(); j++) {
+                inputs.add(new ArrayList<List<Interval>>());
+
+                for (int k = 0; k < inputs1.get(j).size(); k++) {
+                    inputs.get(j).add(new ArrayList<Interval>());
+
+                    for (int g = 0; g < inputs1.get(j).get(k).size(); g++) {
+                        inputs.get(j).get(k).add(inputs1.get(j).get(k).get(g));
+                    }
+                }
             }
 
-            // 调用 Solution 里的静态方法
-            List<Interval> result = Solution.employeeFreeTime(schedule);
-            System.out.println("   Employees' free time = " + display(result));
-            System.out.println("---------------------------------------------------------");
-            caseIndex++;
+            for (int j = 0; j < inputs.size(); j++) {
+                System.out.println(i + ".\tEmployee Schedules:\n");
+
+                for (int s = 0; s < inputs.get(j).size(); s++) {
+                    System.out.println("\t\t" + display(inputs.get(j).get(s)));
+                }
+
+                System.out.println("\n\tEmployees' free time " + display(Solution.employeeFreeTime(inputs.get(j))));
+                System.out.println(new String(new char[100]).replace('\0', '-'));
+                i += 1;
+            }
         }
     }
 }
